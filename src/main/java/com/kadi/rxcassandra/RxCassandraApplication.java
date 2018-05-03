90894e20bb7e9d1fb2d60994b7b370b849aa2789
Aymen Mehri <aymen.mehri@soat.fr>
2018-05-03 17:48:23 +0200

    init project

---
+++ src/main/java/com/kadi/rxcassandra/RxCassandraApplication.java
@@ -0,0 +1,12 @@
+package com.kadi.rxcassandra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RxCassandraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RxCassandraApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kadi/rxcassandra/RxCassandraApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kadi.rxcassandra;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RxCassandraApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
